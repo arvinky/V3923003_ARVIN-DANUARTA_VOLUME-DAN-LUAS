d06cfcdbe0fefd42b82399efbcca003bbd5936c0
arvinky <161785697+arvinky@users.noreply.github.com>
2024-03-02 21:00:01 +0700

    Add files via upload

---
+++ V3923003_ARVIN DANUARTA_LUAS.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import math
+
+def persegi(sisi):
+    return sisi**2
+
+def persegi_panjang(panjang, lebar):
+    return panjang*lebar
+
+def segitiga(alas, tinggi):
+    return 0.5*alas*tinggi
+
+def lingkaran(jari_jari):
+    return math.pi*jari_jari**2
+
+def jajar_genjang(alas, tinggi):
+    return alas*tinggi
+
+def trapesium(sisi_a, sisi_b, tinggi):
+    return 0.5*(sisi_a+sisi_b)*tinggi
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
